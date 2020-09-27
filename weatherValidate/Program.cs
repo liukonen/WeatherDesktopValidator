f3e7201227df1652f3be8c323e36da57a0beb076
Luke <liukonen@gmail.com>
2020-09-27 10:04:13 -0500

    cleanup of code, and upgrade to net core 3.1

--- weatherValidate/Program.cs
+++ weatherValidate/Program.cs
@@ -1,81 +1,56 @@
-﻿using System;
-using System.Collections;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
-using WeatherDesktopSharedCore.Interfaces;
-using WeatherDesktopSharedCore;
-using System.Threading.Tasks;
-using System.Net;
-using System.Web;
 using System.IO;
-using Newtonsoft.Json.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using WeatherDesktopSharedCore.Interfaces;
+using weatherValidate.Properties;
 
 namespace weatherValidate
 {
     class Program
     {
-        static void Main(string[] args)
+ 
+        private static void Main(string[] args)
         {
-
-            var SunObjects = new List<Isrs>();
-            var WeatherObjects = new List<Iweather>();
-            List<ISharedResponse> items = new List<WeatherDesktopSharedCore.Interfaces.ISharedResponse>();
-            items.Add(new MSWeather());
-            items.Add(new NOAAV2());
-            items.Add(new OpenWeather() { APIKey = Properties.Resources.OpenWeatherAPIKey });
-            items.Add(new SunRiseSet());
-            items.Add(new WeatherAPI3());
-            //items.Add(new WeatherUnderGround());
-
-            System.Threading.Tasks.ParallelOptions o = new ParallelOptions();
-            o.MaxDegreeOfParallelism = 4;
-            Parallel.ForEach(items, o, (currentFile) => currentFile.Invoke());
-            foreach (var item in items)
+            new List<Isrs>();
+            new List<Iweather>();
+            List<ISharedResponse> obj = new List<ISharedResponse>
             {
-
-                if (!item.Success()) 
+                new MSWeather(),
+                new NOAAV2(),
+                new OpenWeather
+                {
+                    APIKey = Resources.OpenWeatherAPIKey
+                },
+                new SunRiseSet()
+            };
+            Parallel.ForEach((IEnumerable<ISharedResponse>)obj, new ParallelOptions
+            {
+                MaxDegreeOfParallelism = 4
+            }, (Action<ISharedResponse>)delegate (ISharedResponse currentFile)
+            {
+                currentFile.Invoke();
+            });
+            foreach (ISharedResponse item in obj)
+            {
+                if (!item.Success())
                 {
                     Console.WriteLine(item.ErrorMessages());
-                   // PostToIfTTT(item.GetType().Name, item.ErrorMessages(), item.PostUrl());
+                    PostToIfTTT(item.GetType().Name, item.ErrorMessages(), item.PostUrl());
                 }
-                else {
-
-                    System.Text.StringBuilder sb = new System.Text.StringBuilder();
-                    sb.Append(item.GetType().Name + ": OK - ");
-                    if (item is Iweather) {
-                        var XX = (Iweather)item;
-                        sb.Append("temp:").Append(XX.Temp().ToString()).Append(", ");
-                        //sb.Append("forcast:").Append(XX.ForcastDescription()).Append(", ");
-                        sb.Append("Type:").Append(Enum.GetName(XX.WType().GetType(), XX.WType())).Append(", ");
-                    }
-                    if (item is Isrs) 
-                    {
-                        var XX = (Isrs)item;
-                        sb.Append("").Append(XX.SunRise().ToShortTimeString()).Append(", ");
-                        sb.Append("").Append(XX.SunSet().ToShortTimeString()).Append(", ");
-                    }
-
-
-
-                    Console.WriteLine(sb.ToString());
+                else
+                {
+                    Console.WriteLine(item.GetType().Name + ": OK");
                 }
-                //if (item is Isrs)
-                //{ SunObjects.Add((Isrs)item); }
-                //if (item is Iweather)
-                //{ WeatherObjects.Add((Iweather)item); }
             }
-
-
-
-
-
-
-
         }
 
         private static void PostToIfTTT(string className, string Error, string posturl)
         {
 
-            var httpWebRequest = (HttpWebRequest)WebRequest.Create(Properties.Resources.IFtttUrl);
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create(Resources.IFtttUrl);
             httpWebRequest.ContentType = "application/json";
             httpWebRequest.Method = "POST";
 
